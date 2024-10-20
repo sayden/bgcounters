80014d6d3a3b7c7aa062ca1393e496d50a30e62b
Mario Castro <mariocaster@gmail.com>
2024-10-21 00:39:16 +0200

    Fix test that was still creating a test imag for dev purposes

--- guides_test.go
+++ guides_test.go
@@ -2,7 +2,6 @@ package counters
 
 import (
 	"image/color"
-	"os"
 	"testing"
 
 	"github.com/fogleman/gg"
@@ -47,9 +46,6 @@ func TestGuides(t *testing.T) {
 
 		canvas, err := parsedTemplate.Counters[0].Canvas(true)
 		if assert.NoError(t, err) {
-			f, _ := os.Create("testdata/testing.png")
-			defer f.Close()
-			canvas.EncodePNG(f)
 			testImageContent(t, "testdata/guides_01.png", 1824, canvas)
 		}
 	}
